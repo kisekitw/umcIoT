613576e1285b060941916c87ac56cd56b38d36c2
kisekitw <kisekitw@gmail.com>
2018-12-20 12:46:52 +0800

    add ledplugin to server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const httpServer = require('./servers/http'),
     resources = require('./resources/model'),
-    dhtPlugin = require('./plugins/internal/DHT22SensorPlugin');
+    dhtPlugin = require('./plugins/internal/DHT22SensorPlugin'),
+    ledPlugin = require('./plugins/internal/ledsPlugin');
 
 
-dhtPlugin.start({'simulate': true, 'frequency': 2000});
+dhtPlugin.start({
+    'simulate': true,
+    'frequency': 2000
+});
+ledPlugin.start({
+    'simulate': false,
+    'frequency': 2000
+})
 
 const server = httpServer.listen(resources.pi.port, () => {
     console.log('Your IoT Pi is up and running on port %s', resources.pi.port);
